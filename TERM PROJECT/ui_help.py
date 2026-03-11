67e362a0e1667b16f95a86f0ea194f9718780378
Colby Cordoba <ccordoba@calpoly.edu>
2026-03-11 15:10:44 -0700

    dnoandklj

--- TERM PROJECT/ui_help.py
+++ TERM PROJECT/ui_help.py
@@ -20,11 +20,6 @@ def print_help(s):
     s.write(b"| z | Zero IMU heading (sets current heading to 0)                             |\r\n") 
     s.write(b"| u | Save IMU calibration profile to bno055_calib.bin                         |\r\n")
 
-    # NEED to make it so line not okay does not imediately trigger event or add specifc line not okay error because as it is now it is simply just doing it everytime line not okay. 
-        # After this change it should do all of the major run tasks in one sweep however during the second sequence may need to disable line following until ending sequence in order to keep it from detecting false line.
-    # Bump sensor ISR that does a 200 and enables line following again 
-    # push forwords
-    # Program stops when encoder reach a specific rotation. 
     s.write(b"+---+--------------------------------------------------------------------------+\r\n")
     s.write(b"\r\n>: ")
     pyb.delay(10)
